7b7285ebb56f134f454b697e156a94fe936afa0c
Kenneth Lyerly <kenput3r@gmail.com>
2021-01-27 21:31:52 -0800

    fix typo

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -5,7 +5,7 @@ import styled from "styled-components"
 import { Dialog } from "@reach/dialog"
 import Layout from "../components/layout"
 import Tagline from "../components/tagline"
-import SEO from "../components/SEO"
+import SEO from "../components/seo"
 import "@reach/dialog/styles.css"
 
 const Gallery = ({ data }) => {
