420246731b2e7dd4c4f458bea0c180e62eeaef23
Kenneth Lyerly <kenput3r@gmail.com>
2021-01-27 18:14:00 -0800

    add gallery files

---
+++ src/pages/gallery.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+const Gallery = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Gallery
